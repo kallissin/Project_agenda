960261daaf00d4f11fdaa45a43739a491bfdf784
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-02-03 11:29:03 -0300

    add filter

--- contatos/views.py
+++ contatos/views.py
@@ -4,7 +4,9 @@ from .models import Contato
 from django.core.paginator import Paginator
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('id').filter(
+        mostrar = True
+    )
     paginator = Paginator(contatos, 2)
 
     page = request.GET.get('p')
@@ -15,6 +17,10 @@ def index(request):
 
 def ver_contato(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404
+
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
