940910db2b9932af92f45876c0d331e53ae3f7a7
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-02-09 21:53:19 -0300

    add message ERROR

--- agenda/settings.py
+++ agenda/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +126,13 @@ STATICFILES_DIRS = [
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
+
+# Mensagens
+
+MESSAGE_TAGS = {
+    constants.ERROR: 'alert-danger',
+    constants.DEBUG: 'alert-info',
+    constants.WARNING: 'alert-warning',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+}
--- contatos/views.py
+++ contatos/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from .models import Contato
 from django.core.paginator import Paginator
 from django.db.models import Q, Value
 from django.db.models.functions import Concat
+from django.contrib import messages
 
 def index(request):
+
     contatos = Contato.objects.order_by('id').filter(
         mostrar = True
     )
@@ -30,9 +32,13 @@ def ver_contato(request, contato_id):
 def busca(request):
     termo = request.GET.get('termo')
     
-    if termo is None:
-        raise Http404()
-    
+    if termo is None or not termo:
+        messages.add_message(
+            request,
+            messages.ERROR,
+            'Campo termo não pode ficar vazio'
+        )
+        return redirect('index')
     campos = Concat('nome', Value(' '), 'sobrenome')
     contatos = Contato.objects.annotate(
         nome_completo=campos
